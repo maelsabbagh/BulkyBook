3776eb42c082725c657ebbf832e80a5146f447ef
Mohamed Amr <m.aelsabbagh@gmail.com>
2025-01-27 23:19:50 +0200

    adding DB and a model

---
+++ BulkyBookWeb/Data/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BulkyBookWeb.Data
+{
+    public class ApplicationDbContext:DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options):base(options)
+        {
+            
+        }
+    }
+}
