0e4294e624762987ea26b921318f610e5b6ff87b
Mohamed Amr <m.aelsabbagh@gmail.com>
2025-02-06 21:44:42 +0200

    creating categories table

--- BulkyBookWeb/Data/ApplicationDbContext.cs
+++ BulkyBookWeb/Data/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using BulkyBookWeb.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace BulkyBookWeb.Data
 {
@@ -8,5 +9,7 @@ namespace BulkyBookWeb.Data
         {
             
         }
+
+        public DbSet<Category> Categories { get; set; }
     }
 }
