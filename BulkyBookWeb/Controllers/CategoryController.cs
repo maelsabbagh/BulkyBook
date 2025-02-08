241af9758497766ec6e7725e6495126ca9c6c883
Mohamed Amr <m.aelsabbagh@gmail.com>
2025-02-08 22:49:28 +0200

    implement edit functionality

--- BulkyBookWeb/Controllers/CategoryController.cs
+++ BulkyBookWeb/Controllers/CategoryController.cs
@@ -43,5 +43,43 @@ namespace BulkyBookWeb.Controllers
                 return View(category);
             }
         }
+
+        //GET
+        public IActionResult Edit(int?id)
+        {
+            if (id == null || id == 0) return NotFound();
+
+            var category = _db.Categories.Find(id);
+
+            if(category == null) return NotFound(); 
+
+
+            return View(category);
+        }
+
+        //POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Category category)
+        {
+            if (category.Name == category.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("customError", "Name and display error values cannot be the same");
+            }
+            if (ModelState.IsValid)
+            {
+                _db.Update(category);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View(category);
+            }
+        }
+
+       
+
+
     }
 }
